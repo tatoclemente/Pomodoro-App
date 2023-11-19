cd0af298a51420008189ec48930192d5b1b32b95
tatoclemente <tatoapuros@gmail.com>
2023-11-19 03:05:45 -0300

    Add Header.jsx

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Platform, Text, View, Button, SafeAreaView } from 'react-native';
+import { useState } from 'react';
+import Header from './src/components/Header';
+
+const colors = ["#F7DC6F", "#A2D9CE", "D7BDE2"];
 
 export default function App() {
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(25 * 60);
+  const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "BREAK");
+
   return (
     <SafeAreaView>
       <View style={{ paddingTop: Platform.OS === "android" && 30}}>
-        <Text style={styles.text}>Pomodoro</Text>
+        <Header />
+        <Text style={styles.text}>Time: {time}</Text>
       </View>
     </SafeAreaView>
   );
